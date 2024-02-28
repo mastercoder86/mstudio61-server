a9dd098253e7b8962623d214dfa88e13dff3cb8e
mastercoder86 <budhakumar21@gmail.com>
2024-02-29 01:43:05 +0530

    first demo 7

--- src/main/java/com/m_studio/controller/MyController.java
+++ src/main/java/com/m_studio/controller/MyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.m_studio.dao.CourseRepository;
@@ -43,6 +44,7 @@ public class MyController {
 		return courseService.save(course);
 	}
 	@GetMapping("/")
+	@ResponseBody
 	public String start() {
 		return "hello";
 	}
